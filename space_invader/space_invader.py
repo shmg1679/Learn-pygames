7b11fe6c41e487a5e9411d1670ed3ac2a5fd435c
shmg1679 <shimingh1479@gmail.com>
2023-03-16 04:53:25 -0400

    bullets

--- space_invader/space_invader.py
+++ space_invader/space_invader.py
@@ -7,6 +7,9 @@ pygame.init()
 # creating the screen (width, height) like x and y axis
 screen = pygame.display.set_mode((800, 700))
 
+# background image
+background = pygame.image.load('space.png')
+
 # Title and icon
 pygame.display.set_caption('Space invaders')
 icon = pygame.image.load('space-invaders.png')
@@ -25,7 +28,15 @@ enemyImg = pygame.image.load('alien.png')
 # enemy's random spawn coordinates
 enemy_cord_x = random.randint(0, 736)
 enemy_cord_y = random.randint(60, 200)
-enemy_cord_Xchange = 0
+enemy_cord_Xchange = 0.3
+enemy_cord_Ychange = 20
+
+# bullet
+bulletImg = pygame.image.load('bullet.png')
+bullet_cord_x = 0
+bullet_cord_y = 600
+bullet_cord_Ychange = 0.2
+bullet_state = "ready"
 
 def player(x, y):
     # blit for copying content from one surface to another
@@ -39,12 +50,19 @@ def enemy(x, y):
     # Syntax : blit(source, destination, area=None, special_flags=0) -> Rect
     screen.blit(enemyImg, (x, y))
 
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x+16, y+10))
+
 # Game loop: while its running, it'll go through the events, once its done or you click the close button, it'll quit/close the program
 running = True
 while running:
     
     # RGB color for screen
     screen.fill((255,255,255))
+    # background image
+    screen.blit(background, (0, 0))
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -54,13 +72,20 @@ while running:
         # KEYDOWN means when key is pressed KEYUP means releasing the pressed key
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                player_cord_Xchange = -0.2
+                player_cord_Xchange = -0.4
             if event.key == pygame.K_RIGHT:
-                player_cord_Xchange = 0.2
+                player_cord_Xchange = 0.4
             if event.key == pygame.K_UP:
-                player_cord_Ychange = -0.2
+                player_cord_Ychange = -0.4
             if event.key == pygame.K_DOWN:
-                player_cord_Ychange = 0.2
+                player_cord_Ychange = 0.4
+            if event.key == pygame.K_SPACE:
+                if bullet_state is "ready":
+                    # give bullet the player's coordinate so it'll fire where it was
+                    bullet_cord_x = player_cord_x
+                    bullet_cord_y = player_cord_y
+                    # call function to fire when space is pressed
+                    fire_bullet(bullet_cord_x, bullet_cord_y)
                 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -72,17 +97,42 @@ while running:
     player_cord_x += player_cord_Xchange
     player_cord_y += player_cord_Ychange
     
-    # setting up boundaries
+    
+    # setting up boundaries for player
+    # should not exceed max screen size or go into negative
+    # when it reaches a certain pixel, it'll spawn it back to the edge
+    # x-axis limit
     if player_cord_x <= 0:
         player_cord_x = 0
     elif player_cord_x >= 736:
         # subtract x-axis pixel by the image pixel, otherwise it'll still go off screen
         player_cord_x = 736
-    
+    # y-axis limit
     if player_cord_y <= 0:
         player_cord_y = 0
     elif player_cord_y >= 632:
         player_cord_y = 632
+        
+    # enemies' boundaries
+    enemy_cord_x += enemy_cord_Xchange
+    
+    if enemy_cord_x <= 0:
+        enemy_cord_Xchange = 0.3
+        # only change y coordinates when it hits the wall, otherwise it'll keep going down
+        enemy_cord_y += enemy_cord_Ychange
+    elif enemy_cord_x >= 736:
+        enemy_cord_Xchange = -0.3
+        enemy_cord_y += enemy_cord_Ychange
+    
+    # bullet coordinate resets
+    if bullet_cord_y <= 0:
+        bullet_cord_y = player_cord_y
+        bullet_state = "ready"
+    
+    if bullet_state is "fire":
+        # needs to have the function here in while loop to make it persist in the game
+        fire_bullet(bullet_cord_x, bullet_cord_y)
+        bullet_cord_y -= bullet_cord_Ychange
     
     # calling player and enemy after the screen loads to display itself
     player(player_cord_x, player_cord_y)
