eda0ff82c35380ef75a0f336f785349b56b21ee0
shmg1679 <shimingh1479@gmail.com>
2023-03-17 04:34:40 -0400

    collision somewhat done

--- space_invader/space_invader.py
+++ space_invader/space_invader.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 #initialize the pygame
 pygame.init()
@@ -25,6 +26,7 @@ player_cord_Ychange = 0
 
 # enemy
 enemyImg = pygame.image.load('alien.png')
+enemy_hitbox = enemyImg.get_rect()
 # enemy's random spawn coordinates
 enemy_cord_x = random.randint(0, 736)
 enemy_cord_y = random.randint(60, 200)
@@ -33,10 +35,13 @@ enemy_cord_Ychange = 20
 
 # bullet
 bulletImg = pygame.image.load('bullet.png')
+bullet_hitbox = bulletImg.get_rect()
 bullet_cord_x = 0
 bullet_cord_y = 600
-bullet_cord_Ychange = 0.2
+bullet_cord_Ychange = 0.5
 bullet_state = "ready"
+
+enemies_defeated = 0
 
 def player(x, y):
     # blit for copying content from one surface to another
@@ -49,11 +54,36 @@ def enemy(x, y):
     # in other words, it draws the image in the specified pixels on the screen
     # Syntax : blit(source, destination, area=None, special_flags=0) -> Rect
     screen.blit(enemyImg, (x, y))
+    pygame.draw.rect(enemyImg, (255,255,255), enemy_hitbox, 4)
 
 def fire_bullet(x, y):
     global bullet_state
     bullet_state = "fire"
     screen.blit(bulletImg, (x+16, y+10))
+    pygame.draw.rect(bulletImg, (255,255,255), bullet_hitbox, 4)
+
+
+def collision_triggered(enemy_cord_x, enemy_cord_y, bullet_cord_x, bullet_cord_y):
+    
+    # of the bullet reach a certain mid point of the enemy, it'll trigger collision
+    
+    # distance = math.sqrt((math.pow(enemy_cord_x - enemy_cord_y, 2)) + (math.pow(bullet_cord_x - bullet_cord_y, 2)))
+    # if distance < 27:
+    #     return True
+    # else:
+    #     return False
+    
+    # because mid point and colliderect isn't working, this will have to do
+    if int(bullet_cord_y) == enemy_cord_y:
+        if int(bullet_cord_x) in range(int(enemy_cord_x), int(enemy_cord_x+64)):
+            print(bullet_cord_y, " ", enemy_cord_y)
+            print(bullet_cord_x, " ", enemy_cord_x, " ", enemy_cord_x+64)
+            return True
+        else:
+            return False
+    else:
+        return False
+        
 
 # Game loop: while its running, it'll go through the events, once its done or you click the close button, it'll quit/close the program
 running = True
@@ -80,7 +110,7 @@ while running:
             if event.key == pygame.K_DOWN:
                 player_cord_Ychange = 0.4
             if event.key == pygame.K_SPACE:
-                if bullet_state is "ready":
+                if bullet_state == "ready":
                     # give bullet the player's coordinate so it'll fire where it was
                     bullet_cord_x = player_cord_x
                     bullet_cord_y = player_cord_y
@@ -129,10 +159,20 @@ while running:
         bullet_cord_y = player_cord_y
         bullet_state = "ready"
     
-    if bullet_state is "fire":
+    if bullet_state == "fire":
         # needs to have the function here in while loop to make it persist in the game
         fire_bullet(bullet_cord_x, bullet_cord_y)
         bullet_cord_y -= bullet_cord_Ychange
+    
+    # collision
+    collision =  collision_triggered(enemy_cord_x, enemy_cord_y, bullet_cord_x, bullet_cord_y)
+    if collision:
+        bullet_cord_x = player_cord_x
+        bullet_state = "ready"
+        enemies_defeated += 1
+        enemy_cord_x = random.randint(0, 736)
+        enemy_cord_y = random.randint(60, 200)
+        print(enemies_defeated)
     
     # calling player and enemy after the screen loads to display itself
     player(player_cord_x, player_cord_y)
